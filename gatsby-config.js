423a67d89e95d2af9d4bb01401b2255fbca1d82f
Amber Lyons <amber.lyons38@gmail.com>
2023-02-28 23:44:45 -0500

    Added pull from class demo site

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,15 @@ module.exports = {
         path: `${__dirname}/blog`,
       }
     },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // This type will contain remote schema Query type
+        typeName: "DrupalGraqhQL",
+        // This is field under which it's accessible
+        fieldName: "Drupal",
+        url: https://csc496f22demo.tldr.dev/graphql,
+      }
+    },
   ],
 };
